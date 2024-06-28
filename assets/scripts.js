db8527eafaa71df376b8709b22dbad27e3928964
SamoshkinaAnastasia <162181193+SamoshkinaAnastasia@users.noreply.github.com>
2024-06-28 12:10:34 +0300

    Add files via upload

---
+++ assets/scripts.js
@@ -0,0 +1,16 @@
+const slides = document.querySelector('.slides');
+const slideWidth = document.querySelector('.slide').clientWidth;
+let currentIndex = 0;
+
+setInterval(() => {
+    currentIndex = (currentIndex + 1) % slides.children.length;
+    slides.style.transition = 'transform 0.5s ease-in-out';
+    slides.style.transform = `translateX(-${slideWidth * currentIndex}px)`;
+
+    if (currentIndex === 0) {
+        setTimeout(() => {
+            slides.style.transition = 'none';
+            slides.style.transform = 'translateX(0)';
+        }, 500);
+    }
+}, 7000);
